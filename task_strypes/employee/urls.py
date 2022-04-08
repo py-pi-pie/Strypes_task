c3fb5d8e08f88a6b71dc7134b13313dcfbde76d3
py-pi-pie <gabonexxx@gmail.com>
2022-04-08 18:12:57 +0300

    employee update added, templates

--- task_strypes/employee/urls.py
+++ task_strypes/employee/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-from employee.views import EmployeesApi, UploadXLSX, AddEmployee
+from employee.views import EmployeesApi, UploadXLSX, AddEmployee, EditEmployee
 
 employee_patterns = [
     # url(r'^get-all-employees/', EmployeesApi.as_view(), name='employees'),
-    url(r'^get-all-employees/', AddEmployee.as_view(), name='employees'),
+    url(r'^get-all-employees/', EditEmployee.as_view(), name='employees'),
     ]
--- task_strypes/employee/views.py
+++ task_strypes/employee/views.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from openpyxl import load_workbook
@@ -12,6 +12,9 @@ import os
 
 from employee.models import Employee
 import re
+
+from django.http import HttpResponseBadRequest, HttpResponse
+from django.urls import reverse
 
 MOBILE_REGEX = re.compile(r'\d+')
 
@@ -108,7 +111,7 @@ class UploadXLSX(APIView):
 
 class AddEmployee(APIView):
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'base_t.html'
+    template_name = 'create_employee.html'
 
     class EmployeeSerializer(serializers.Serializer):       # DRY principle
         first_name = serializers.CharField()
@@ -152,19 +155,71 @@ class AddEmployee(APIView):
 
 class EditEmployee(APIView):
     renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'base_t.html'
+    template_name = 'update_employee.html'
+
+    class EmployeeSerializer(serializers.Serializer):       # DRY principle
+        first_name = serializers.CharField()
+        last_name = serializers.CharField()
+        mobile_num = serializers.IntegerField()
+        start_date = serializers.DateField()
+        position = serializers.CharField()
+        salary = serializers.IntegerField()
+        employee_id = serializers.CharField()
+
+    def get(self, request):
+        return Response()
 
     def post(self, request):
-        # DRY
-        POSITION_FIELD_REPRESENTATION = {'ceo': 'CEO', 'junior developer': 'junior_dev',
-                                         'senior developer': 'senior_dev', 'team lead': 'team_lead',
-                                         'project manager': 'project_manager'}
 
-        employee = {'first_name': request.data.get('first_name'),
-                    'last_name': request.data.get('last_name'),
-                    'mobile_num': int(request.data.get('mobile_num')),
-                    'start_date': datetime.strptime(request.data.get('start_date'), '%Y-%m-%d').date(),
-                    'position': POSITION_FIELD_REPRESENTATION.get(request.data.get('position').lower()),
-                    'salary': int(request.data.get('salary')),
-                    'employee_id': request.data.get('employee_id'),
-                    }
+        employee_to_update_set = Employee.objects.filter(employee_id=request.data.get('id_to_update'))
+
+        if employee_to_update_set:
+            employee_to_update = employee_to_update_set[0]
+        # DRY
+            POSITION_FIELD_REPRESENTATION = {'ceo': 'CEO', 'junior developer': 'junior_dev',
+                                             'senior developer': 'senior_dev', 'team lead': 'team_lead',
+                                             'project manager': 'project_manager'}
+
+            first_name = request.data.get('first_name') \
+                if request.data.get('first_name') else employee_to_update.first_name
+            last_name = request.data.get('last_name') \
+                if request.data.get('last_name') else employee_to_update.last_name
+            mobile_num = int(request.data.get('mobile_num')) \
+                if request.data.get('mobile_num') else employee_to_update.mobile_num
+            start_date = datetime.strptime(request.data.get('start_date'), '%Y-%m-%d').date() \
+                if request.data.get('start_date') else employee_to_update.start_date
+            position = POSITION_FIELD_REPRESENTATION.get(request.data.get('position').lower()) \
+                if request.data.get('position') else employee_to_update.position
+            salary = int(request.data.get('salary')) \
+                if request.data.get('salary') else employee_to_update.salary
+            employee_id = request.data.get('employee_id') \
+                if request.data.get('employee_id') else employee_to_update.employee_id
+
+            employee = {'first_name': first_name,
+                        'last_name': last_name,
+                        'mobile_num': mobile_num,
+                        'start_date': start_date,
+                        'position': position,
+                        'salary': salary,
+                        'employee_id': employee_id,
+                        }
+
+            serializer_employees = self.EmployeeSerializer(data=employee)
+
+            if serializer_employees.is_valid():
+                employee_to_update_set.update(first_name=employee.get('first_name'),
+                                              last_name=employee.get('last_name'),
+                                              mobile_num=employee.get('mobile_num'),
+                                              start_date=employee.get('start_date'),
+                                              position=employee.get('position'),
+                                              salary=employee.get('salary'),
+                                              employee_id=employee.get('employee_id')
+
+                                              )
+                message = f'Employee with ID {request.data.get("id_to_update")} has been updated \n ' \
+                          f'<p><a href="{reverse("employees")}">Home Page</a></p>'
+                return HttpResponse(message, status=status.HTTP_201_CREATED)
+
+        message = f'Employee with ID {request.data.get("id_to_update")} NOT found'
+        return HttpResponseBadRequest(message, status=status.HTTP_404_NOT_FOUND)
+
